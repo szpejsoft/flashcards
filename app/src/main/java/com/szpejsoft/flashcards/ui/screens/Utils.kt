059ac7dc2f32c97c8e2b9eaa6fc0395cb2c0913a
Szpejsoft <szpejsoft@gmail.com>
2026-03-01 17:07:32 +0100

    libs updated

--- app/src/main/java/com/szpejsoft/flashcards/ui/screens/Utils.kt
+++ app/src/main/java/com/szpejsoft/flashcards/ui/screens/Utils.kt
@@ -1,13 +1,20 @@
 package com.szpejsoft.flashcards.ui.screens
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlin.random.Random
 
-fun <T> MutableList<T>.replaceWith(elements: List<T>) {
-    clear()
-    addAll(elements)
-}
 
 fun <T> List<T>.getRandom(): T {
     val index = Random.nextInt(this.size)
     return this[index]
+}
+
+fun <T> MutableCollection<T>.replaceWith(values: Collection<T>) {
+    clear()
+    addAll(values)
 }
